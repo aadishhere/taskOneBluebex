2167c90def6b1f65e1809f6f501ae2fa15053ee7
Aadish Jain <aadishhere@gmail.com>
2025-05-12 11:41:52 +0530

    Initial Commit

---
+++ taskOneBluebex/taskOneBluebexApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskOneBluebexApp.swift
+//  taskOneBluebex
+//
+//  Created by Aadish Jain on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct taskOneBluebexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
